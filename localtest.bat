04ace591097cac5821294d674b528d152ec64306
0xarchit <0xarchit@gmail.com>
2026-08-17 02:23:09 +0530

    fix localtest command

--- localtest.bat
+++ localtest.bat
@@ -1,9 +1,9 @@
 @echo off
 (
-  uv run mypy
-  uv run ruff format --check .
-  uv run ruff check .
-  uv run pylint tests tg_logging_handler
-  uv run flake8 tests tg_logging_handler
-  uv run pytest --cov=tg_logging_handler
+  uv run mypy || exit /b 1
+  uv run ruff format --check . || exit /b 1
+  uv run ruff check . || exit /b 1
+  uv run pylint tests tg_logging_handler || exit /b 1
+  uv run flake8 tests tg_logging_handler || exit /b 1
+  uv run pytest --cov=tg_logging_handler || exit /b 1
 ) > local.log 2>&1
